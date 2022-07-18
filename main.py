a0c40949e59524d31caad297b7386baa9fe84b24
Marco Rodríguez <mrodriguez@truelogic.io>
2022-07-17 21:44:46 -0500

    add function to get last known position of a satellite by id at a given time

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import click
 import json
 import typer
 
-from sqlmodel import Session
+from dateutil.parser import parse
+
+from sqlmodel import col, select, Session
+from sqlmodel.sql.expression import Select, SelectOfScalar
+
+SelectOfScalar.inherit_cache = True  # type: ignore
+Select.inherit_cache = True  # type: ignore
 
 from model import create_table, StarlinkRecord, engine
 
@@ -11,18 +17,22 @@ app = typer.Typer()
 
 @app.command(name="initialize_db")
 def init_db():
+    """Create the table `starlinkrecord` in the DB"""
     create_table()
 
 
 @app.command(name="load_data")
 def load_data():
+    """Load data into the DB from the provided json file"""
+
     with open("data/starlink_historical_data.json", "r") as json_file:
         data = json.load(json_file)
+
     with Session(engine) as session:
         for row in data:
             creation_date = row["spaceTrack"]["CREATION_DATE"]
-            latitude = row["latitude"]
-            longitude = row["longitude"]
+            latitude = str(row["latitude"])
+            longitude = str(row["longitude"])
             satellite_id = row["id"]
 
             starlink_record = StarlinkRecord(
@@ -32,7 +42,24 @@ def load_data():
                 satellite_id=satellite_id,
             )
             session.add(starlink_record)
+
         session.commit()
+
+
+@app.command(name="get_by_id")
+def get_position_by_id(id: str, datetime: str) -> str:
+    """Get the last known position of a satellite by id at a given time
+    """
+    with Session(engine) as session:
+        statement = select(StarlinkRecord).where(
+            col(StarlinkRecord.satellite_id) == id,
+            col(StarlinkRecord.creation_date) <= parse(datetime),
+        )
+        result = session.exec(statement).first()
+        if result:
+            print(f"({result.latitude}, {result.longitude})")
+        else:
+            print("Not found")
 
 
 @app.command()
--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,7 @@
 from pydantic.schema import datetime
 from typing import Optional
 
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import create_engine, Field, SQLModel
 
 POSTGRES_URI = "postgresql://postgres:postgres@0.0.0.0:5432/postgres"
 
@@ -9,8 +9,8 @@ POSTGRES_URI = "postgresql://postgres:postgres@0.0.0.0:5432/postgres"
 class StarlinkRecord(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     creation_date: datetime
-    latitude: Optional[float] = None
-    longitude: Optional[int] = None
+    latitude: Optional[str] = None  # All latitudes in the json file are float or None
+    longitude: Optional[str] = None  # All longitudes in the json file are int or None
     satellite_id: str = Field(index=True)
 
 
